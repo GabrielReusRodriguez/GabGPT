66da8271f513f08a88bb235eb0be9de992a838b5
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2026-05-08 23:30:52 +0200

    Refactorizacion para que el paso de flags a llama-server funcione con variables de entorno

--- all/GabGPT/llama.cpp/scripts/entrypoint.sh
+++ all/GabGPT/llama.cpp/scripts/entrypoint.sh
@@ -3,6 +3,7 @@
 #TODO: Configurar los chat-models y los stops...
 
 # Ejecutamos el servidor
+: '
 exec /app/llama.cpp/build/bin/llama-server \
     --model "${LLAMACPP_MODEL_2_RUN}" \
     --host 0.0.0.0 \
@@ -11,3 +12,6 @@ exec /app/llama.cpp/build/bin/llama-server \
     --flash-attn on \
     --n-gpu-layers "${GPU_CFG_NUM_GPU_LAYERS}" \
     --ctx-size "${LLAMACPP_MODEL_CTX_SIZE}"
+'
+
+exec /app/llama.cpp/build/bin/llama-server
