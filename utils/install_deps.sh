cbb243d687d893168c04a439067163e3ea358c41
Gabriel Reus Rodriguez <gabrielin@gmail.com>
2026-05-09 13:58:26 +0200

    añadido la instalacion de git y git-lfs para poder descargar modelosd e hugging face

--- utils/install_deps.sh
+++ utils/install_deps.sh
@@ -27,5 +27,4 @@ curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o 
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 # Instalo docker en si
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin git git-lfs
