bcaea02f8f00edc816d09b46e9a9161386142f06
Gabriel Reus <gabriel.reus@pm.me>
2026-05-26 00:33:55 +0200

    Eliminado el env del script de importacion de variables de entorno

--- all/GabGPT/llama.cpp/scripts/import_env.sh
+++ all/GabGPT/llama.cpp/scripts/import_env.sh
@@ -8,7 +8,5 @@ echo "Modelo a cargar: $LLAMA_ARG_ALIAS - ${GABGPT_GUEST_MODELS_FOLDER}/${GABGPT
 
 # Exporto todos las variables de entorno del fichero de entorno correspondiente.
 export $(grep -v '^#' ${GABGPT_GUEST_MODELS_FOLDER}/${GABGPT_MODEL_FAMILY}/${GABGPT_MODEL_FILE}.env | xargs)
-#export $(grep -v '^#' /app/models/Qwen/Qwen-3-14B-Q4_K_M.env  | xargs)
 
-env
 
